b0b487daf874aba9a6b2df919de5479555d1e516
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-01-17 06:23:32 -0700

    should be final

--- Program.cs
+++ Program.cs
@@ -3,33 +3,51 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello, World!");
 
-        string howMany = "";
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        System.Console.Write("How many dice rolls would you like to simulate? ");
+        if (!int.TryParse(Console.ReadLine(), out int totalRolls) || totalRolls <= 0)
+        {
+            Console.WriteLine("Please enter a positive integer.");
+            return;
+        }
+
+        RollemDice roller = new RollemDice();
+        int[] rollResults = roller.SimulateRolls(totalRolls);
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine($"Total number of rolls = {totalRolls}.");
+
+        for (int i = 0; i < rollResults.Length; i++)
+        {
+            int percentage = (rollResults[i] * 100) / totalRolls;
+            System.Console.WriteLine($"{i + 2}: {new string('*', percentage)}");
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
+
+class RollemDice
+{
+    public int[] SimulateRolls(int totalRolls)
+    {
+        int[] rollCounts = new int[11];
         Random rnd = new Random();
-        System.Console.WriteLine("How many time do you want to roll the dice?");
-        howMany = System.Console.ReadLine();
-     
-        List<int> diceNumStorage = new List<int>();
 
-
-
-        //Basic Syntax for a for loop below
-
-        for (int i = 0; i < int.Parse(howMany); i++)
+        for (int i = 0; i < totalRolls; i++)
         {
             int dice1 = rnd.Next(1, 7);
             int dice2 = rnd.Next(1, 7);
             int sum = dice1 + dice2;
-            diceNumStorage.Add(sum);
 
-           
+            rollCounts[sum - 2]++;
         }
 
-        for (int i = 0; i < diceNumStorage.Count; i++)
-        {
-            Console.WriteLine(diceNumStorage[i]);
-        }
-
+        return rollCounts;
     }
+
+
 }
--- obj/Debug/net8.0/mission#2.AssemblyInfo.cs
+++ obj/Debug/net8.0/mission#2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mission#2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de35f40ffa9c8a5a4bbc38a7113304cafe5839ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("mission#2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mission#2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
