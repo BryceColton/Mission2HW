ffa4dd68ec677d6f0fb6ce019eb43839b036e7e0
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-01-17 06:41:30 -0700

    added comments

--- DiceClass.cs
+++ DiceClass.cs
@@ -8,17 +8,20 @@ namespace mission_2
 {
     internal class DiceClass
     {
-            public int[] SimulateRolls(int totalRolls)
+// In this constructor it passes the totalsRolls variable in and assigns the roll counts to an array with 11 spots
+        public int[] SimulateRolls(int totalRolls)
             {
                 int[] rollCounts = new int[11];
                 Random rnd = new Random();
 
+            // This for loop is the simulation logic where it randomly assigns a number between 1 and 6. I have 7 so it goes up to 7.
                 for (int i = 0; i < totalRolls; i++)
                 {
                     int dice1 = rnd.Next(1, 7);
                     int dice2 = rnd.Next(1, 7);
                     int sum = dice1 + dice2;
 
+                // Roll counts sum - 2 shows that the index is going from [0 to 10] so it has to minus two to keep up with 2 - 12 combinations
                     rollCounts[sum - 2]++;
                 }
 
--- Program.cs
+++ Program.cs
@@ -5,23 +5,30 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        // This line welcomes the user
         Console.WriteLine("Welcome to the dice throwing simulator!");
 
+        // This line asks how many times the user wants to simulate throwing the dice
         Console.Write("How many dice rolls would you like to simulate? ");
+
+//This next line adds exception handling, the out allows function to return more than one output
         if (!int.TryParse(Console.ReadLine(), out int totalRolls) || totalRolls <= 0)
         {
             Console.WriteLine("Please enter a positive integer.");
             return;
         }
 
+// This line creates a new DiceClass object and gives access to the simulate rolls function
         DiceClass roller = new DiceClass();
         int[] rollResults = roller.SimulateRolls(totalRolls);
 
+// This next line present conclusion of the simulation with the results 
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         Console.WriteLine($"Total number of rolls = {totalRolls}.");
 
+
+// This for loop prints out the * asterisk for each combination, this new string function seemed the simplest to use and understand.
         for (int i = 0; i < rollResults.Length; i++)
         {
             int percentage = (rollResults[i] * 100) / totalRolls;
