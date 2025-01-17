4e230d828e6039ebce5d96e4e6047a0b88b60e9a
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-01-17 06:34:31 -0700

    fixed the compiled

--- DiceClass.cs
+++ DiceClass.cs
@@ -8,5 +8,23 @@ namespace mission_2
 {
     internal class DiceClass
     {
+            public int[] SimulateRolls(int totalRolls)
+            {
+                int[] rollCounts = new int[11];
+                Random rnd = new Random();
+
+                for (int i = 0; i < totalRolls; i++)
+                {
+                    int dice1 = rnd.Next(1, 7);
+                    int dice2 = rnd.Next(1, 7);
+                    int sum = dice1 + dice2;
+
+                    rollCounts[sum - 2]++;
+                }
+
+                return rollCounts;
+            }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
-﻿
+﻿using System;
+using mission_2;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
 
-        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("Welcome to the dice throwing simulator!");
 
-        System.Console.Write("How many dice rolls would you like to simulate? ");
+        Console.Write("How many dice rolls would you like to simulate? ");
         if (!int.TryParse(Console.ReadLine(), out int totalRolls) || totalRolls <= 0)
         {
             Console.WriteLine("Please enter a positive integer.");
             return;
         }
 
-        RollemDice roller = new RollemDice();
+        DiceClass roller = new DiceClass();
         int[] rollResults = roller.SimulateRolls(totalRolls);
 
-        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
-        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
-        System.Console.WriteLine($"Total number of rolls = {totalRolls}.");
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {totalRolls}.");
 
         for (int i = 0; i < rollResults.Length; i++)
         {
@@ -26,28 +28,7 @@ internal class Program
             System.Console.WriteLine($"{i + 2}: {new string('*', percentage)}");
         }
 
-        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
 }
 
-class RollemDice
-{
-    public int[] SimulateRolls(int totalRolls)
-    {
-        int[] rollCounts = new int[11];
-        Random rnd = new Random();
-
-        for (int i = 0; i < totalRolls; i++)
-        {
-            int dice1 = rnd.Next(1, 7);
-            int dice2 = rnd.Next(1, 7);
-            int sum = dice1 + dice2;
-
-            rollCounts[sum - 2]++;
-        }
-
-        return rollCounts;
-    }
-
-
-}
